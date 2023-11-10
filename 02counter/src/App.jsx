17246ec1ec91f4545fceba7c16b0763ce5c28aec
Neha00011 <112271756+Neha00011@users.noreply.github.com>
2023-11-10 14:57:51 +0530

    use state

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+  //let counter = 15;
+  const addValue = () => {
+    //console.log("value added", counter);
+    //counter = counter + 1;
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>counter value:{counter}</h2>
+      <button onClick={addValue}>add value{counter}</button>
+      <br></br>
+      <button onClick={removeValue}>remove value{counter}</button>
+      <br></br>
+      <p>footer:{counter}</p>
+    </>
+  );
+}
+
+export default App;
+
+//assignment:never go less than 0 or above 20
