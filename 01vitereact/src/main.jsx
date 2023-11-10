0f128cc9a32450772591d3a3f5aa023ba7136299
Neha00011 <112271756+Neha00011@users.noreply.github.com>
2023-11-10 14:26:30 +0530

    basic set up

---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+const anotheruser = "neha";
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click to visit google"
+  // anotheruser
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
