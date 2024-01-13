42b8b11d8def97ee621afce5dc914ad5d31e7619
Neha00011 <112271756+Neha00011@users.noreply.github.com>
2024-01-13 22:31:40 +0530

    redux toolkit

---
+++ 11_reduxToolkit/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Todos from "./Components/Todos";
+import AddTodo from "./Components/AddTodo";
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
